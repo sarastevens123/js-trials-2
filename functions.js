1880cffe4547ec741af63feb4e63a9229463f7c9
Sara Stevens <sarastevens@Saras-MacBook-Air.local>
2022-09-24 17:21:04 -0700

    finished functions.js

--- functions.js
+++ functions.js
@@ -2,12 +2,33 @@
 
 // 1. isHometown
 
-// Define your function here
+const isHometown = town => town === 'San Francisco';
 
 // 2. getFullName
 
-// Define your function here
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+
+    const subTotal = basePrice * (1 + tax); 
+
+    let fee = 0
+
+    if (state === 'CA') {
+        fee = 0.03 * subTotal;
+    } else if (state === 'PA') {
+        fee = 2;
+    } else if (state === 'MA') {
+        if (basePrice <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
+
+    return (subTotal + fee);
+
+}
+
